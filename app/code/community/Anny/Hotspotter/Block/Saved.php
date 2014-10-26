77742f00efb50c552bb936e3e78f0b3a6a368e8c
Aneurin Barker Snook <anny@by-anny.co.uk>
2014-10-26 21:19:21 +0000

    add block for embed-by-id

---
+++ app/code/community/Anny/Hotspotter/Block/Saved.php
@@ -0,0 +1,27 @@
+<?php
+
+class Anny_Hotspotter_Block_Saved extends Mage_Core_Block_Abstract {
+	protected function _widget() {
+		$id = $this->getData('id');
+		if ($id) {
+			$widget = Mage::getModel('widget/widget_instance')->load($id);
+			if ($widget->getId() || $widget->getType() != 'hotspotter/hotspot') {
+				return $widget;
+			}
+		}
+		return null;
+	}
+
+	protected function _toHtml() {
+		$widget = $this->_widget();
+		if (! $widget) {
+			return '';
+		}
+		$block = $this->getLayout()->createBlock(
+			$widget->getType(),
+			$widget->getTitle(),
+			$widget->getWidgetParameters()
+		);
+		return $block->toHtml();
+	}
+}
